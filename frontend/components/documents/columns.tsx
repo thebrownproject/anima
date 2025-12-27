908d4e268ac70169ef25a5f5c487db58a563a467
thebrownproject <fraserbrown@live.com>
2025-12-27 12:22:33 +1100

    style: fix header row height by removing button padding

--- frontend/components/documents/columns.tsx
+++ frontend/components/documents/columns.tsx
@@ -75,7 +75,7 @@ export const columns: ColumnDef<Document>[] = [
       <Button
         variant="ghost"
         onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        className="-ml-3 group font-normal"
+        className="-ml-3 group font-normal h-auto py-0"
       >
         Name
         <SortIcon isSorted={column.getIsSorted()} />
@@ -116,7 +116,7 @@ export const columns: ColumnDef<Document>[] = [
       <Button
         variant="ghost"
         onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        className="group font-normal"
+        className="group font-normal h-auto py-0"
       >
         <SortIcon isSorted={column.getIsSorted()} />
         Date
