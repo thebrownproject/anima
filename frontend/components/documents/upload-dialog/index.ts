e33dc8146df90c78de1d908b1eda0d8b0c5e0834
thebrownproject <fraserbrown@live.com>
2025-12-28 09:00:50 +1100

    chore: remove upload-dialog barrel file, use direct imports

--- frontend/components/documents/upload-dialog/index.ts
+++
@@ -1,4 +0,0 @@
-// frontend/components/documents/upload-dialog/index.ts
-export { UploadDialogTrigger } from './upload-dialog-trigger'
-export { UploadDialogContent } from './upload-dialog-content'
-export type { CustomField, UploadStep, ExtractionMethod } from '@/types/upload'
